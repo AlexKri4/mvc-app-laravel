e2ad454b79e57d55729f36f07586838989ca2262
Kri4ik <madhater4716@gmail.com>
2022-10-18 13:40:33 +0300

    feat: update category CRUD

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -340,8 +340,8 @@ class ComposerStaticInit9f47916e20338a842f000d050b1cba38
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
